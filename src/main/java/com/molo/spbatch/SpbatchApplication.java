1c065c84b7ad8f9c26cdd919e3c658a4336e89ff
Mouhamadou Moustapha LO <mouhamadoulo39@gmail.com>
2026-09-13 19:31:11 +0200

    first commit

---
+++ src/main/java/com/molo/spbatch/SpbatchApplication.java
@@ -0,0 +1,13 @@
+package com.molo.spbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/molo/spbatch/SpbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molo.spbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
